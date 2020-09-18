0532a9c7d1fa45dbc76eca3f1d7b6154b8df9c70
Michael Babozhko <goodiny777>
2020-09-18 16:24:11 +0300

    Updates

---
+++ precache-manifest.f95301a308fd00e7ab2f4470a36715d5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0278f15b7f10cd8a5a4f955836a32c25",
+    "url": "/mbworks/index.html"
+  },
+  {
+    "revision": "cb72207b869ec9b8e546",
+    "url": "/mbworks/static/css/main.3713ab59.chunk.css"
+  },
+  {
+    "revision": "a67c5563c868de4ba274",
+    "url": "/mbworks/static/js/2.4a24541e.chunk.js"
+  },
+  {
+    "revision": "cb72207b869ec9b8e546",
+    "url": "/mbworks/static/js/main.0a6a7bc1.chunk.js"
+  },
+  {
+    "revision": "b4ba4ad93cdfe6850f40",
+    "url": "/mbworks/static/js/runtime~main.b09cdc11.js"
+  },
+  {
+    "revision": "594c828179ccb41f8cb3d747ae7de9ea",
+    "url": "/mbworks/static/media/cert_analitics.594c8281.jpg"
+  },
+  {
+    "revision": "f2afc2516a5327ca123d5d0706f43dcb",
+    "url": "/mbworks/static/media/cert_css.f2afc251.jpg"
+  },
+  {
+    "revision": "8810205222c0e3ba435e76255bc38fc6",
+    "url": "/mbworks/static/media/cert_hackeru.88102052.jpg"
+  },
+  {
+    "revision": "a4bda59ce260fd5948c6e5fe6cef960d",
+    "url": "/mbworks/static/media/cert_html.a4bda59c.jpg"
+  },
+  {
+    "revision": "713009ef1cd6d96aad9a752cdcdebce2",
+    "url": "/mbworks/static/media/cert_java.713009ef.jpg"
+  },
+  {
+    "revision": "6b9fbd0f3af67c2fe5c033bb2d6c3998",
+    "url": "/mbworks/static/media/cert_jquery.6b9fbd0f.jpg"
+  },
+  {
+    "revision": "d1851d540099a9af1b9c66420197b359",
+    "url": "/mbworks/static/media/cert_js.d1851d54.jpg"
+  },
+  {
+    "revision": "b18f5efd065c44f4f879397adb671606",
+    "url": "/mbworks/static/media/cert_node_js.b18f5efd.png"
+  },
+  {
+    "revision": "aa66bfd165aba332ed9f7a6fb738ffc3",
+    "url": "/mbworks/static/media/cert_sql.aa66bfd1.jpg"
+  },
+  {
+    "revision": "5a26ace8b84d5621dec2540d05b13ae4",
+    "url": "/mbworks/static/media/certificate_elements_of_ai.5a26ace8.png"
+  },
+  {
+    "revision": "bdf2cdd56dc7483f93e1ca6e875f4a5f",
+    "url": "/mbworks/static/media/ds_logo.bdf2cdd5.png"
+  },
+  {
+    "revision": "45fffff315924e7897503706be70b71f",
+    "url": "/mbworks/static/media/header-bg.45fffff3.jpg"
+  },
+  {
+    "revision": "f6b1aeca8c54839b880964f7edb84087",
+    "url": "/mbworks/static/media/highschool.f6b1aeca.jpg"
+  },
+  {
+    "revision": "a8e5b3884f7b36c53b973f5a0fd58205",
+    "url": "/mbworks/static/media/ibm_hackaton.a8e5b388.jpeg"
+  },
+  {
+    "revision": "1bae0e46d48f7c41cf56a897abe27037",
+    "url": "/mbworks/static/media/karma_logo.1bae0e46.png"
+  },
+  {
+    "revision": "f52c24cbb0c3bc909bc7a8fae3f92cce",
+    "url": "/mbworks/static/media/linkedin_icon.f52c24cb.png"
+  },
+  {
+    "revision": "566cf29ca5ce1b72f4ba1a661b83dc60",
+    "url": "/mbworks/static/media/telegram_icon.566cf29c.png"
+  },
+  {
+    "revision": "aa10612a84d66d98fe522fd089694ed5",
+    "url": "/mbworks/static/media/weekup_logo.aa10612a.jpg"
+  }
+]);
